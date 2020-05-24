bf32d25c164c0a30827257d9f967f779225f45c2
levistan <2503arjun@gmail.com>
2020-05-25 01:58:20 +0530

    added bivariate bar graph

--- plotter.py
+++ plotter.py
@@ -8,9 +8,16 @@ def bi_variate_graph(data,column_names):
     feature1=st.selectbox("Select first feature",column_names,key="selector")
     feature2=st.selectbox("Select second feature",column_names,key="selector1")
     flag_vis=st.checkbox("visualize",value=False)
-
+    bar_flag=False
+    scatter_flag=False
     if flag_vis:
-        fig=px.scatter(data,x=feature1,y=feature2)
-        st.plotly_chart(fig)
+        bar_flag=st.checkbox("Do you want a bar plot?",value=False)
+        scatter_flag=st.checkbox("Do you want a scatter plot?",value=False)
+    if bar_flag:
+        fig_bar=px.bar(data,x=feature1,y=feature2)
+        st.plotly_chart(fig_bar)
+    if scatter_flag:
+        fig_scatter=px.scatter(data,x=feature1,y=feature2)
+        st.plotly_chart(fig_scatter)
 
     
